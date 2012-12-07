0ea002d94ca27d2a12b0d765c65e7a85a1ab39c2
vaishaal <vaishaal@vaishaals-MacBook-Air.local>
2012-12-06 22:02:44 -0800

    added support for midterms

--- scrape_ninja.py
+++ scrape_ninja.py
@@ -23,21 +23,31 @@ def scrape_hkn(abv="CS",course="70"):
     prof_year[current_semester] = current_tuple
     return prof_year 
 ways = ["","(solution)","solution"] 
-def scrape_ninja(course="70",test="Final",department="COMPSCI",abv="CS"):
+
+def scrape_ninja(course="70",test="Midterm 1",department="COMPSCI",abv="CS"):
     prof_year = scrape_hkn(abv,course)
     base_url = "http://media.ninjacourses.com/var/exams/1/{0}/{1}%20{2}%20-%20{3}%20{4}%20-%20{5}%20-%20{6}%20{7}.pdf" 
     exists = {}  
     for semester,profs in prof_year.iteritems():
             sem,year = tuple(semester.split()) 
             for prof in profs:
+                
                 for way in ways:
                     try:
                         prof = prof.split()[-1] 
-                        url = base_url.format(department,abv,course,sem,year,prof,test,way) 
-                        if not way:
-                            url = url[:-7] + ".pdf"
+                        test_split = test.split() 
+                        if len(test_split) == 1: 
+                            url = base_url.format(department,abv,course,sem,year,prof,test,way) 
+                            if not way:
+                                url = url[:-7] + ".pdf"
+                        elif len(test_split) == 2: 
+                            url = base_url.format(department,abv,course,sem,year,prof,test_split[0],test_split[1]) 
+                            if way:
+                                url = url[:-4] + "%20"+way + url[-4:] 
+
+                            
                         urllib2.urlopen(url) 
-                        exists[str(profs + (way,))+" "+semester ] = url
+                        exists[str(profs + (test,way))+" "+semester ] = url
                     except Exception as e:
                         #print e  
                         pass
@@ -45,7 +55,11 @@ def scrape_ninja(course="70",test="Final",department="COMPSCI",abv="CS"):
         print url
         urllib.urlretrieve(url,info+ ".pdf")
 
+tests = ['Midterm','Midterm 1','Midterm 2', 'Midterm 3','Final']
+
 if (len(sys.argv) > 1):
-    scrape_ninja(sys.argv[1]) 
+    for test in tests: 
+        scrape_ninja(sys.argv[1],test) 
 else:
-    scrape_ninja()
+    for test in tests: 
+        scrape_ninja("70",test)
